38e611cd3d6a2c8dca8a7ac01b8ecb06242e506a
ryzhykE <stoyatboy@gmail.com>
2017-10-12 20:39:50 +0300

    Update book_shop.sql

--- book_shop.sql
+++ book_shop.sql
@@ -1,352 +1,116 @@
--- phpMyAdmin SQL Dump
--- version 4.4.15.5
--- http://www.phpmyadmin.net
---
--- Хост: 127.0.0.1:3306
--- Время создания: Окт 12 2017 г., 17:44
--- Версия сервера: 5.7.11
--- Версия PHP: 7.0.4
+CREATE TABLE `book` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`title` varchar(255) NOT NULL,
+	`discription` TEXT NOT NULL,
+	`price` varchar(255) NOT NULL,
+	`img` varchar(255) NOT NULL DEFAULT 'no_image.jpg',
+	`discount` FLOAT NOT NULL DEFAULT '0',
+	PRIMARY KEY (`id`)
+);
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-SET time_zone = "+00:00";
+CREATE TABLE `author` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`name_author` varchar(255) NOT NULL,
+	PRIMARY KEY (`id`)
+);
 
+CREATE TABLE `genre` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`name_genre` varchar(255) NOT NULL AUTO_INCREMENT,
+	PRIMARY KEY (`id`)
+);
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE `book_to_ganre` (
+	`id_book` int(11) NOT NULL,
+	`id_genre` int(11) NOT NULL
+);
 
---
--- База данных: `book_shop`
---
+CREATE TABLE `book_to_author` (
+	`id_book` int(11) NOT NULL,
+	`id_author` int(11) NOT NULL
+);
 
--- --------------------------------------------------------
+CREATE TABLE `client` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`first_name` varchar(100) NOT NULL,
+	`last_name` varchar(100) NOT NULL,
+	`login` varchar(255) NOT NULL,
+	`password` varchar(100) NOT NULL,
+	`hash` varchar(255) NOT NULL,
+	`role` enum('admin','user') NOT NULL,
+	`discount` FLOAT(10) NOT NULL DEFAULT '0',
+	`active` enum('yes','no') NOT NULL,
+	PRIMARY KEY (`id`)
+);
 
---
--- Структура таблицы `author`
---
+CREATE TABLE `cart` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`id_book` int(11) NOT NULL,
+	`id_client` int(11) NOT NULL,
+	`count` int(11) NOT NULL,
+	PRIMARY KEY (`id`)
+);
 
-CREATE TABLE IF NOT EXISTS `author` (
-  `id` int(11) NOT NULL,
-  `name_author` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `orders` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`id_client` int(11) NOT NULL AUTO_INCREMENT,
+	`id_status` int(11) NOT NULL AUTO_INCREMENT,
+	`time` DATE NOT NULL,
+	`id_payment` int NOT NULL,
+	`total_price` FLOAT(10) NOT NULL,
+	PRIMARY KEY (`id`)
+);
 
--- --------------------------------------------------------
+CREATE TABLE `payment` (
+	`id` int(5) NOT NULL AUTO_INCREMENT,
+	`name_pay` int(5) NOT NULL,
+	PRIMARY KEY (`id`)
+);
 
---
--- Структура таблицы `book`
---
+CREATE TABLE `order_status` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`name_statut` int(11) NOT NULL,
+	PRIMARY KEY (`id`)
+);
 
-CREATE TABLE IF NOT EXISTS `book` (
-  `id` int(11) NOT NULL,
-  `title` varchar(255) NOT NULL,
-  `discription` text NOT NULL,
-  `price` varchar(255) NOT NULL,
-  `img` varchar(255) NOT NULL DEFAULT 'no_image.jpg',
-  `discount` float NOT NULL DEFAULT '0'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+CREATE TABLE `history_book` (
+	`id` int NOT NULL AUTO_INCREMENT,
+	`name` varchar(255) NOT NULL,
+	`author` varchar(255) NOT NULL,
+	`genre` varchar(255) NOT NULL,
+	`price` int(11) NOT NULL,
+	PRIMARY KEY (`id`)
+);
 
--- --------------------------------------------------------
+CREATE TABLE `orders_full_info` (
+	`id` int(11) NOT NULL AUTO_INCREMENT,
+	`id_order` int(11) NOT NULL,
+	`id_book` int(11) NOT NULL,
+	`title_book` varchar(255) NOT NULL,
+	`price` varchar(255) NOT NULL,
+	`discount_book` TEXT NOT NULL,
+	PRIMARY KEY (`id`)
+);
 
---
--- Структура таблицы `book_to_author`
---
+ALTER TABLE `book_to_ganre` ADD CONSTRAINT `book_to_ganre_fk0` FOREIGN KEY (`id_book`) REFERENCES `book`(`id`);
 
-CREATE TABLE IF NOT EXISTS `book_to_author` (
-  `id_book` int(11) NOT NULL,
-  `id_author` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+ALTER TABLE `book_to_ganre` ADD CONSTRAINT `book_to_ganre_fk1` FOREIGN KEY (`id_genre`) REFERENCES `genre`(`id`);
 
--- --------------------------------------------------------
+ALTER TABLE `book_to_author` ADD CONSTRAINT `book_to_author_fk0` FOREIGN KEY (`id_book`) REFERENCES `book`(`id`);
 
---
--- Структура таблицы `book_to_ganre`
---
+ALTER TABLE `book_to_author` ADD CONSTRAINT `book_to_author_fk1` FOREIGN KEY (`id_author`) REFERENCES `author`(`id`);
 
-CREATE TABLE IF NOT EXISTS `book_to_ganre` (
-  `id_book` int(11) NOT NULL,
-  `id_genre` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+ALTER TABLE `cart` ADD CONSTRAINT `cart_fk0` FOREIGN KEY (`id_book`) REFERENCES `book`(`id`);
 
--- --------------------------------------------------------
+ALTER TABLE `cart` ADD CONSTRAINT `cart_fk1` FOREIGN KEY (`id_client`) REFERENCES `client`(`id`);
 
---
--- Структура таблицы `cart`
---
+ALTER TABLE `orders` ADD CONSTRAINT `orders_fk0` FOREIGN KEY (`id_client`) REFERENCES `client`(`id`);
 
-CREATE TABLE IF NOT EXISTS `cart` (
-  `id` int(11) NOT NULL,
-  `id_book` int(11) NOT NULL,
-  `id_client` int(11) NOT NULL,
-  `count` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+ALTER TABLE `orders` ADD CONSTRAINT `orders_fk1` FOREIGN KEY (`id_status`) REFERENCES `order_status`(`id`);
 
--- --------------------------------------------------------
+ALTER TABLE `orders` ADD CONSTRAINT `orders_fk2` FOREIGN KEY (`id_payment`) REFERENCES `payment`(`id`);
 
---
--- Структура таблицы `client`
---
+ALTER TABLE `orders_full_info` ADD CONSTRAINT `orders_full_info_fk0` FOREIGN KEY (`id_order`) REFERENCES `orders`(`id`);
 
-CREATE TABLE IF NOT EXISTS `client` (
-  `id` int(11) NOT NULL,
-  `first_name` varchar(100) NOT NULL,
-  `last_name` varchar(100) NOT NULL,
-  `login` varchar(255) NOT NULL,
-  `password` varchar(100) NOT NULL,
-  `role` enum('admin','user') NOT NULL,
-  `discount` float NOT NULL DEFAULT '0',
-  `active` enum('yes','no') NOT NULL DEFAULT 'yes'
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+ALTER TABLE `orders_full_info` ADD CONSTRAINT `orders_full_info_fk1` FOREIGN KEY (`id_book`) REFERENCES `book`(`id`);
 
--- --------------------------------------------------------
-
---
--- Структура таблицы `genre`
---
-
-CREATE TABLE IF NOT EXISTS `genre` (
-  `id` int(11) NOT NULL,
-  `name_genre` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `history_book`
---
-
-CREATE TABLE IF NOT EXISTS `history_book` (
-  `id` int(11) NOT NULL,
-  `name` varchar(255) NOT NULL,
-  `author` varchar(255) NOT NULL,
-  `genre` varchar(255) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `orders`
---
-
-CREATE TABLE IF NOT EXISTS `orders` (
-  `id` int(11) NOT NULL,
-  `id_book` int(11) NOT NULL,
-  `id_client` int(11) NOT NULL,
-  `id_status` int(11) NOT NULL,
-  `time` date NOT NULL,
-  `id_payment` int(11) NOT NULL,
-  `quantity` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `order_full_info`
---
-
-CREATE TABLE IF NOT EXISTS `order_full_info` (
-  `id` int(11) NOT NULL,
-  `id_book` int(11) NOT NULL,
-  `title_book` varchar(255) NOT NULL,
-  `price` varchar(255) NOT NULL,
-  `discount` text NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `order_status`
---
-
-CREATE TABLE IF NOT EXISTS `order_status` (
-  `id` int(11) NOT NULL,
-  `name_statut` int(11) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
--- --------------------------------------------------------
-
---
--- Структура таблицы `payment`
---
-
-CREATE TABLE IF NOT EXISTS `payment` (
-  `id` int(5) NOT NULL,
-  `name_pay` int(5) NOT NULL
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-
---
--- Индексы сохранённых таблиц
---
-
---
--- Индексы таблицы `author`
---
-ALTER TABLE `author`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `book`
---
-ALTER TABLE `book`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `book_to_author`
---
-ALTER TABLE `book_to_author`
-  ADD KEY `book_to_author_fk0` (`id_book`),
-  ADD KEY `book_to_author_fk1` (`id_author`);
-
---
--- Индексы таблицы `book_to_ganre`
---
-ALTER TABLE `book_to_ganre`
-  ADD KEY `book_to_ganre_fk0` (`id_book`),
-  ADD KEY `book_to_ganre_fk1` (`id_genre`);
-
---
--- Индексы таблицы `cart`
---
-ALTER TABLE `cart`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `cart_fk0` (`id_book`),
-  ADD KEY `cart_fk1` (`id_client`);
-
---
--- Индексы таблицы `client`
---
-ALTER TABLE `client`
-  ADD PRIMARY KEY (`id`),
-  ADD UNIQUE KEY `login` (`login`);
-
---
--- Индексы таблицы `genre`
---
-ALTER TABLE `genre`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `history_book`
---
-ALTER TABLE `history_book`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `orders`
---
-ALTER TABLE `orders`
-  ADD PRIMARY KEY (`id`),
-  ADD KEY `orders_fk0` (`id_book`),
-  ADD KEY `orders_fk1` (`id_client`),
-  ADD KEY `orders_fk2` (`id_status`),
-  ADD KEY `orders_fk3` (`id_payment`);
-
---
--- Индексы таблицы `order_full_info`
---
-ALTER TABLE `order_full_info`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `order_status`
---
-ALTER TABLE `order_status`
-  ADD PRIMARY KEY (`id`);
-
---
--- Индексы таблицы `payment`
---
-ALTER TABLE `payment`
-  ADD PRIMARY KEY (`id`);
-
---
--- AUTO_INCREMENT для сохранённых таблиц
---
-
---
--- AUTO_INCREMENT для таблицы `author`
---
-ALTER TABLE `author`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT для таблицы `book`
---
-ALTER TABLE `book`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT для таблицы `cart`
---
-ALTER TABLE `cart`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT для таблицы `client`
---
-ALTER TABLE `client`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT для таблицы `genre`
---
-ALTER TABLE `genre`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT для таблицы `history_book`
---
-ALTER TABLE `history_book`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT для таблицы `orders`
---
-ALTER TABLE `orders`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT для таблицы `order_full_info`
---
-ALTER TABLE `order_full_info`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT для таблицы `order_status`
---
-ALTER TABLE `order_status`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
---
--- AUTO_INCREMENT для таблицы `payment`
---
-ALTER TABLE `payment`
-  MODIFY `id` int(5) NOT NULL AUTO_INCREMENT;
---
--- Ограничения внешнего ключа сохраненных таблиц
---
-
---
--- Ограничения внешнего ключа таблицы `book_to_author`
---
-ALTER TABLE `book_to_author`
-  ADD CONSTRAINT `book_to_author_fk0` FOREIGN KEY (`id_book`) REFERENCES `book` (`id`),
-  ADD CONSTRAINT `book_to_author_fk1` FOREIGN KEY (`id_author`) REFERENCES `author` (`id`);
-
---
--- Ограничения внешнего ключа таблицы `book_to_ganre`
---
-ALTER TABLE `book_to_ganre`
-  ADD CONSTRAINT `book_to_ganre_fk0` FOREIGN KEY (`id_book`) REFERENCES `book` (`id`),
-  ADD CONSTRAINT `book_to_ganre_fk1` FOREIGN KEY (`id_genre`) REFERENCES `genre` (`id`);
-
---
--- Ограничения внешнего ключа таблицы `cart`
---
-ALTER TABLE `cart`
-  ADD CONSTRAINT `cart_fk0` FOREIGN KEY (`id_book`) REFERENCES `book` (`id`),
-  ADD CONSTRAINT `cart_fk1` FOREIGN KEY (`id_client`) REFERENCES `client` (`id`);
-
---
--- Ограничения внешнего ключа таблицы `orders`
---
-ALTER TABLE `orders`
-  ADD CONSTRAINT `orders_fk0` FOREIGN KEY (`id_book`) REFERENCES `book` (`id`),
-  ADD CONSTRAINT `orders_fk1` FOREIGN KEY (`id_client`) REFERENCES `client` (`id`),
-  ADD CONSTRAINT `orders_fk2` FOREIGN KEY (`id_status`) REFERENCES `order_status` (`id`),
-  ADD CONSTRAINT `orders_fk3` FOREIGN KEY (`id_payment`) REFERENCES `payment` (`id`);
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
